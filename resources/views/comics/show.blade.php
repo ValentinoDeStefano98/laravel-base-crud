7c20bb8a7f60d0dccdf52b729a5a00a81ec01017
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-06-01 15:20:38 +0200

    style added

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
     <div class="container text-center">
-        <h2 class="text-primary text-center">{{$comic->title}}</h2>
-        <img src="{{$comic->thumb}}" alt="copertina">
+        <h2 class="text-primary text-center py-2">{{$comic->title}}</h2>
+        <img class="py-4" src="{{$comic->thumb}}" alt="copertina">
         <p>{{$comic->description}}</p>
-        <p>{{$comic->price}}$</p>
+        <p>Prezzo attuale: {{$comic->price}}$</p>
         <h4>{{$comic->series}}</h4>
-        <h6>{{$comic->sale_date}}</h6>
+        <h6>Data di rilascio: {{$comic->sale_date}}</h6>
         <h6>{{$comic->type}}</h6>  
     </div>
     
